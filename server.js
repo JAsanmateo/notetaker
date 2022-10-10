4704f4a8ce7b426508c54e643b946f2870b455b2
Jose San Mateo <info@joseasanmateo.com>
2022-10-09 18:29:36 -0700

    Testing app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +14,7 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname + '/public')));
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
 
